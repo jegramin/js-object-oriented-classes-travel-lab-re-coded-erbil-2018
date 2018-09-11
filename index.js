074c76cc7a303f7559e8cd5f4f83491f3a1467fd
Mohammed Ameen <jegroamin@gmail.com>
2018-09-11 20:08:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+class Driver{
+  constructor(name,startDate){
+    this.name=name;
+    this.startDate=new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(year){
+    let endDate = new Date(year,1,1);
+    let totalYears = (endDate  - this.startDate) / (365*24*60*60*1000);
+    return parseInt(totalYears)+1;
+  }
+}
 
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.endingLocation = endingLocation;
+    this.beginningLocation = beginningLocation;
+  }
+  blocksTravelled(){
+    return this.endingLocation - this.beginningLocation;
+  }
+  beginningLocation(){
+    
+  }
+  endingLocation(){
+    
+  }
+}
